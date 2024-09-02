a45f03bf73007376a01d9abc45df147c83b5b2f0
Quang <159697999+progressive-newbie263@users.noreply.github.com>
2024-09-02 21:31:11 +0700

    post javascript code

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,69 @@ import Form from './component/Form.jsx';
 
 
 function App() {
+  const [mortgageType, setMortgageType] = useState('');
+  const [amount, setAmount] = useState("");
+  const [term, setTerm] = useState('');
+  const [rate, setRate] = useState("");
+  const [monthlyPayment, setMonthlyPayment] = useState("");
+  const [totalPayment, setTotalPayment] = useState("");
+  const [isCalculated, setIsCalculated] = useState(false); //for toggling 
+
+  const calculateRepayments = () => {
+    const principal = parseFloat(amount);
+    const years = parseInt(term);
+    const interestRate = parseFloat(rate) / 100;
+    const monthlyRate = interestRate / 12;
+
+    let monthlyRepayment = 0;
+    let totalRepayment = 0;
+
+    if (mortgageType === 'Repayment') {
+      // Repayment mortgage calculation
+      monthlyRepayment = (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -years * 12));
+      totalRepayment = monthlyRepayment * years * 12;
+    } else if (mortgageType === 'Interest Only') {
+      // Interest-only mortgage calculation
+      monthlyRepayment = principal * monthlyRate;
+      totalRepayment = (monthlyRepayment * years * 12) + principal;
+    }
+
+    setMonthlyPayment(monthlyRepayment.toFixed(2));
+    setTotalPayment(totalRepayment.toFixed(2));
+    setIsCalculated(true); 
+  };
 
   return (
     <>
-      <div className='flex flex-col justify-center h-fit w-96 bg-white 
-        rounded-2xl my-10 
+      {/* i will select media screen for laptop is lg/1024px */}
+      <div className='flex flex-col justify-center h-fit w-96 
+      bg-white rounded-2xl my-10
+      lg:flex-row lg:w-[900px]
       '>
-        <Form />
-        <Result />
+        <Form
+          mortgageType={mortgageType}
+          setMortgageType={setMortgageType}
+          amount={amount}
+          setAmount={setAmount}
+          term={term}
+          setTerm={setTerm}
+          rate={rate}
+          setRate={setRate}
+          calculateRepayments={calculateRepayments}
+        />
+        
+        <Result
+          monthlyPayment={monthlyPayment}
+          totalPayment={totalPayment}
+          isCalculated={isCalculated}
+        />
       </div>
 
       <Footer />
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
+
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,83 +1,94 @@
-import React, {useState} from "react";
+import React from "react";
 import Calculator from "./svg-component/Calculator.jsx";
-//import Select from "react-select"; //lib for mortgage type selection
 
-function Form() {
-  const [mortgageType, setMortgageType] = useState('');
-
+function Form({
+  mortgageType,
+  setMortgageType,
+  amount,
+  setAmount,
+  term,
+  setTerm,
+  rate,
+  setRate,
+  calculateRepayments
+}) {
   const handleChange = (e) => {
     setMortgageType(e.target.value);
   };
-  
-  return (
-    <form className="flex flex-col px-6 py-8 text-slate-500">
-      <span className="text-2xl font-bold text-slate-700">
-        Mortgage Calculator
-      </span>
-      
-      <button className="w-fit underline mt-1 mb-4
-        hover:text-slate-900
-      ">
-        Clear All
-      </button>
 
+  return (
+    <form className="flex flex-col px-6 py-8 text-slate-500 lg:w-[450px]">
+      <div className="flex flex-col lg:flex-row lg:justify-between">
+        <span className="text-2xl font-bold text-slate-700">
+          Mortgage Calculator
+        </span>
+
+        <button className="w-fit underline mt-1 mb-4 hover:text-slate-900">
+          Clear All
+        </button>
+      </div>
+      
       {/* amount label */}
       <label htmlFor="amount">Mortgage Amount</label>
       <div className="inline-block relative mt-2 mb-4">
-        <span className="absolute my-[1px] mx-[1px] py-[7px] 
-        px-4 rounded bg-sky-100 ">
-            <b>&#163;</b>
+        <span className="absolute my-[1px] mx-[1px] py-[7px] px-4 rounded bg-sky-100 ">
+          <b>&#163;</b>
         </span>
 
         <input className="border border-slate-500 w-full rounded h-10 
           hover:border-slate-900 cursor-pointer pl-12 font-bold text-slate-700" 
-          type="number" id="amount" />
+          type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
       </div>
-
 
       {/* flex col for phone, flex-row for large screen */}
-      <div className="flex flex-col">
+      <div className="flex flex-col lg:flex-row lg:">
         {/* mortgage term */}
-        <label htmlFor="term">Mortgage Amount</label>
-        <div className="inline-block relative mt-2 mb-4">
-          <span className="absolute right-0 my-[1px] mx-[1px] py-[7px] 
-          px-4 rounded bg-sky-100">
-              <b>years</b>
-          </span>
+        <div className="lg:w-[190px] lg:mr-[20px]">
+          <label htmlFor="term">Mortgage Term</label>
+          <br/>
+          <div className="inline-block relative mt-2 mb-4">
+            <span className="absolute right-0 my-[1px] mx-[1px] py-[7px] 
+            px-4 rounded bg-sky-100">
+                <b>years</b>
+            </span>
 
-          <input className="border border-slate-500 w-full rounded h-10 
-            hover:border-slate-900 cursor-pointer pl-4 font-bold text-slate-700" 
-            type="number" id="term" />
+            <input className="border border-slate-500 w-[336px] lg:w-full rounded h-10 
+              hover:border-slate-900 cursor-pointer pl-4 font-bold text-slate-700  
+            " 
+              type="number" id="term" value={term} onChange={(e) => setTerm(e.target.value)} />
+          </div>
         </div>
+        
 
         {/* interest rate label*/}
-        <label htmlFor="rate">Interest Rate</label>
-        <div className="inline-block relative mt-2 mb-4">
-          <span className="absolute right-0 my-[1px] mx-[1px] py-[7px] 
-          px-4 rounded bg-sky-100">
-              <b>%</b>
-          </span>
+        <div className="lg:w-[190px]">
+          <label htmlFor="rate">Interest Rate</label>
+          <br/>
+          <div className="inline-block relative mt-2 mb-4">
+            <span className="absolute right-0 my-[1px] mx-[1px] py-[7px] 
+            px-4 rounded bg-sky-100">
+                <b>%</b>
+            </span>
 
-          <input className="border border-slate-500 w-full rounded h-10 
-            hover:border-slate-900 cursor-pointer pl-4 font-bold text-slate-700" 
-            type="number" id="rate" />
+            <input className="border border-slate-500 w-[336px] lg:w-full rounded h-10 
+              hover:border-slate-900 cursor-pointer pl-4 font-bold text-slate-700" 
+              type="number" id="rate" value={rate} onChange={(e) => setRate(e.target.value)} />
+          </div>
         </div>
       </div>
 
-      {/*  */}
+      {/* Mortgage Type */}
       <div>
         <label>Mortgage Type</label>
         <div>
-          <label className="block border border-solid p-2 rounded
-        border-slate-500 hover:border-[#CCCE3B] mt-2 cursor-pointer">
+          <label className="block border border-solid p-2 rounded border-slate-500 hover:border-[#CCCE3B] mt-2 cursor-pointer">
             <input type="radio" value="Repayment" checked={mortgageType === 'Repayment'}
               onChange={handleChange} className="mr-2"
             />
             <span className="font-bold text-slate-700">Repayment</span>
           </label>
 
-          <label className="block border border-solid p-2 rounded
-        hover:border-[#CCCE3B] border-slate-500 mt-2 cursor-pointer">
+          <label className="block border border-solid p-2 rounded hover:border-[#CCCE3B] border-slate-500 mt-2 cursor-pointer">
             <input type="radio" value="Interest Only" checked={mortgageType === 'Interest Only'}
               onChange={handleChange} className="mr-2"
             />
@@ -86,9 +97,10 @@ function Form() {
         </div>
       </div>
 
-      {/* custom lime didn't work, so i will use a converted hex color of it here (CCCE3B) */}
-      <button className="flex bg-[#CCCE3B] hover:bg-lime-200 justify-center rounded-full py-2 mt-6"
-        
+      <button
+        type="button"
+        className="flex bg-[#CCCE3B] hover:bg-lime-200 justify-center rounded-full py-2 mt-6"
+        onClick={calculateRepayments}
       >
         <span className="mr-4"><Calculator/></span>
         Calculate Repayments
